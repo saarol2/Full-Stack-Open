cc38db97c3ea8207b1611ad343efdc8d2d768ef2
Oliver Saari <saarioliver02@gmail.com>
2024-06-05 17:16:22 +0300

    Anekdootit

---
+++ osa1/anekdootit/src/App.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+
+const Anecdote = ({ anecdote, votes }) => (
+  <div>
+    <div>{anecdote}</div>
+    <div>Has {votes} votes</div>
+  </div>
+)
+
+const Button = ({ handleClick, text }) => {
+  return (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+  )
+}
+
+const BestAnecdote = ({anecdotes, votes}) =>{
+  const maxVotes = Math.max(...votes)
+  const index = votes.indexOf(maxVotes)
+  return (
+    <div>
+      <h2>Anecdote with the most votes</h2>
+      <Anecdote anecdote={anecdotes[index]} votes={maxVotes} />
+    </div>
+  )
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+  const initialVotes = new Array(anecdotes.length).fill(0) 
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(initialVotes)
+  
+  const Random = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length);
+    setSelected(randomIndex);
+  }
+  const Vote = () =>{
+    const newVotes = [...votes]
+    newVotes[selected] += 1
+    setVotes(newVotes)
+  }
+
+  return (
+    <div>
+      <h2>Anecdote of the day</h2>
+      <Anecdote anecdote={anecdotes[selected]} votes={votes[selected]} />
+      <Button handleClick={Vote} text = "vote" />
+      <Button handleClick={Random} text = "next anecdote" />
+      <BestAnecdote anecdotes={anecdotes} votes={votes} />
+    </div>
+  )
+}
+
+export default App
